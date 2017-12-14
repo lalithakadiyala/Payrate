d34c93f18750b23751469c9a6a9e4613640e9470
gkadiyala <gkadiyala@gkadiyala-glaptop.roam.corp.google.com>
2017-12-14 11:31:14 -0800

    first comment

---
+++ loop.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class loop {
+	
+	public static void main(String[] args){
+		int i;
+		int A;
+		
+		for (i=1; i<=10; i++) {
+			A= i*i;
+
+			System.out.print( +i  ) ;
+		 System.out.println( "   "  +A);
+		}
+			
+	}
+}
+
+
+
